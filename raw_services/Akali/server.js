67f41d5cda5339aa81157a7b320feb07a8ed9202
Võ Chí Trung <147219006+ChisTrun@users.noreply.github.com>
2025-01-21 12:46:38 +0700

    init

---
+++ raw_services/Akali/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { init, fetchBusinessManagers, fetchCandidates, fetchBusinessManagersbyIDs } = require('./db-connect');
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+
+app.get('/businessManagers', async (req, res) => {
+    res.send(await fetchBusinessManagers())
+});
+
+app.get('/candidates', async (req, res) => {
+    res.send(await fetchCandidates())
+});
+
+app.post('/account/company/many', async (req, res) => {
+    const ids = req.body.ids;
+    // Example body for POST request to /account/company/many
+    // {
+    //     "ids": ["id1", "id2", "id3"]
+    // }
+    res.send(await fetchBusinessManagersbyIDs(ids))
+});
+
+
+app.listen(process.env.Port, () => {
+    console.log(`Server is running on http://localhost:${process.env.Port}`);
+});
